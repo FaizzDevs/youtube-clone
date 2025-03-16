834449b5ebc38e208c1335a8bcac4df04bebe758
FaizzDevs <faizdevs@gmail.com>
2025-03-16 20:38:48 +0700

    update

--- src/modules/studio/ui/sections/video-sections.tsx
+++ src/modules/studio/ui/sections/video-sections.tsx
@@ -48,7 +48,11 @@ const  VideoSectionSuspense = () => {
                                     <TableCell>
                                         <div className="flex items-center gap-4">
                                             <div className="relative aspect-video w-36 shrink-0">
-                                                <VideoThumbnail />
+                                                <VideoThumbnail 
+                                                    imageUrl={video.thumbnailUrl} 
+                                                    previewUrl={video.previewUrl}
+                                                    title={video.title}
+                                                    duration={video.duration || 0} />
                                             </div>
                                         </div>
                                     </TableCell>
