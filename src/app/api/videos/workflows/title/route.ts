f14562b6e544169bbe76c49a8ae495741f9b07a7
FaizzDevs <faizdevs@gmail.com>
2025-04-21 08:24:42 +0700

    update code

--- src/app/api/videos/workflows/title/route.ts
+++ src/app/api/videos/workflows/title/route.ts
@@ -8,12 +8,20 @@ interface InputType {
   videoId: string
 };
 
+const TITLE_SYSTEM_PROMPT = `Your task is to generate an SEO-focused title for a Youtube video based on its transcript. Please follow these guidelines:
+- Be conside but descriptive, using relevant keywords to improve discoverability.
+- Highlight the most compelling or unique aspect of the video content.
+- Avoid jargon or overly complex language unless it directly supports searchibility.
+- Use action-oriented phrasing or clear value propositions where applicable.
+- Ensure the title is 3-8 words long and no more than 100 characters.
+- ONLY return the title as plain text. Do not add quotes or any additional formatting.`;
+
 export const { POST } = serve(
   async (context) => {
     const input = context.requestPayload as InputType;
     const { userId, videoId } = input;
 
-    const video = context.run("get-video", async () => {
+    const video = await context.run("get-video", async () => {
       const [existingVideo] = await db
         .select()
         .from(videos)
@@ -27,22 +35,42 @@ export const { POST } = serve(
         }
 
         return existingVideo;
-    })  
+    })
+    
+    const { body } = await context.api.openai.call(
+      "generate-title",
+      {
+        token: process.env.OPENAI_API_KEY!,
+        operation: "chat.completions.create",
+        body: {
+          model: "gpt-4o",
+          messages: [
+            {
+              role: "system",
+              content: TITLE_SYSTEM_PROMPT,
+            },
+            {
+              role: "user",
+              content: "Hi everyone!!, in this I will be building Youtube Clone'"
+            }
+          ],
+        },
+      }
+    );
+    
+    const title = body.choices?.[0]?.message?.content || "Default Title";
 
     await context.run("update-video", async () => {
       await db 
         .update(videos)
         .set({
-          title: "New Title"
+          title: title || video.title,
         })
         .where(and(
-          eq(videos.id, videoId),
+          eq(videos.id, video.id),
           eq(videos.userId, userId),
         ))
     })
-
-    await context.run("second-step", () => {
-      console.log("second step ran")
-    })
+    
   }
 );
