8a8c519e4e4b8ec54c0188733bbc9ac5ac8a6986
FaizzDevs <faizdevs@gmail.com>
2025-05-11 09:04:21 +0700

    update code

--- src/modules/video-views/server/procedures.ts
+++ src/modules/video-views/server/procedures.ts
@@ -1,11 +1,38 @@
+import { db } from "@/db";
+import { videoViews } from "@/db/schema";
 import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
 import { z } from "zod";
+import { and, eq } from "drizzle-orm";
 
 export const videoViewsRouter = createTRPCRouter({
     create: protectedProcedure
+        //memastikan login sesuai akun/userId
         .input(z.object({ videoId: z.string().uuid() }))
         .mutation(async ({ input, ctx }) => {
             const { videoId } = input;
             const { id: userId } = ctx.user;
-        })
-})
+
+            //memanggil data pada database
+            const[existingVideoView] = await db
+                .select()
+                .from(videoViews)
+                .where(
+                    and(
+                        eq(videoViews.videoId, videoId,),
+                        eq(videoViews.userId, userId),
+                    )
+                );
+
+            if(existingVideoView){
+                return existingVideoView;
+            }
+
+            //menambahkan value ke database
+            const [createdVideoView] = await db
+                .insert(videoViews)
+                .values({userId, videoId})
+                .returning();
+
+                return createdVideoView
+        }),
+});
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -3,6 +3,8 @@ import { studioRouter } from '@/modules/studio/server/procedures';
 import { createTRPCRouter } from '../init';
 import { categoriesRouter } from '@/modules/categories/server/procedures';
 import { videosRouter } from '@/modules/videos/server/procedures';
+// import { videoViews } from '@/db/schema';
+import { videoViewsRouter } from '@/modules/video-views/server/procedures';
 
 export const appRouter = createTRPCRouter({
   // hello: baseProcedure.query(() => {
@@ -11,6 +13,7 @@ export const appRouter = createTRPCRouter({
   studio: studioRouter,
   categories: categoriesRouter,
   videos: videosRouter,
+  videoView: videoViewsRouter,
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
