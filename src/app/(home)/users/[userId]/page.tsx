c9f535f3187ab08915f7b216da1fb69774c3af16
FaizzDevs <faizdevs@gmail.com>
2025-06-23 16:59:59 +0700

    update code

--- src/app/(home)/users/[userId]/page.tsx
+++ src/app/(home)/users/[userId]/page.tsx
@@ -1,3 +1,5 @@
+import { DEFAULT_LIMIT } from "@/constants";
+import { VideosSection } from "@/modules/users/ui/sections/videos-section";
 import { UserView } from "@/modules/users/ui/views/user-view";
 import { HydrateClient, trpc } from "@/trpc/server";
 
@@ -9,11 +11,13 @@ const Page = async ({ params }: PageProps) => {
     const { userId } = await params;
 
     // connect to server users.getOne
-    void trpc.users.getOne.prefetch({ id: userId });
+    void trpc.users.getOne.prefetch({ id: userId }); // prefetch => untuk jumlah data kecil seperti userId
+    void trpc.videos.getMany.prefetchInfinite({ userId, limit: DEFAULT_LIMIT }) // prefetch => untuk jumlah data besar dan ada loadmore
 
     return (
         <HydrateClient>
             <UserView userId={userId} />
+            <VideosSection userId={userId} />
         </HydrateClient>
     )
 }
